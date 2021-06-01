357ea3aafd2b5a1b3f7387897647a434368db058
lukaluka04378 <62738464+lukaluka04378@users.noreply.github.com>
2021-06-01 15:18:20 +0200

    Add files via upload

---
+++ Auto-skola-master/com/autoskola/Autoskola.java
@@ -0,0 +1,92 @@
+package com.autoskola;
+
+import java.util.ArrayList;
+import java.lang.Math; 
+
+public class Autoskola {
+	private String naziv;
+	private int cena;
+	private int lokacija;
+	private ArrayList<Ucenik> ucenici;
+	private ArrayList<Instruktor> instruktori;
+	
+	public Autoskola(String naziv, int cena, int lokacija, ArrayList<Ucenik> ucenici, ArrayList<Instruktor> instruktori)
+	{
+		this.naziv = naziv;
+		this.cena = cena;
+		this.lokacija = lokacija;
+		this.ucenici = new ArrayList<Ucenik>();
+		this.ucenici = ucenici;
+		this.instruktori = new ArrayList<Instruktor>();
+		this.instruktori = instruktori;
+		/*
+		for(int i = 0;i < ucenici.length;++i)
+		{
+			this.ucenici[i] = new Ucenik(ucenici[i].getIme(), ucenici[i].getPrezime(), ucenici[i].getDatumRodjenja(), ucenici[i].getPol(), ucenici[i].getId(), ucenici[i].getNapredak(), ucenici[i].getBrojCasova(), ucenici[i].getBrojPalihIspita(), ucenici[i].getPolozenaTeorija(), ucenici[i].getPolozenaPraksa(), ucenici[i].getInstruktor());			
+		}
+		*/
+	}
+	
+	String getNaziv()
+	{
+		return naziv;
+	}
+	int getCena()
+	{
+		return cena;
+	}
+	int getLokacija()
+	{
+		return lokacija;
+	}
+
+	void dodajInstruktora(Instruktor instruktor)
+	{
+		instruktori.add(instruktor);
+	}
+	
+	void dodajUcenika(Ucenik ucenik)
+	{
+		ucenici.add(ucenik);
+	}
+	
+	void polaganjeTeorije(int kolicina)
+	{
+		for(int i = 0;i < kolicina;++i)
+		{
+			if(Math.random() * 10 + 1 < 8)
+				ucenici.get(i).polozioTeoriju();
+			else
+				ucenici.get(i).pao();
+		}
+	}
+	void polaganjePrakse()
+	{
+		double tezina = Math.random() * 3 + 1; //1-strogi, 2-srednje, 3-popustljivi
+		if(Math.random() * 10 + 1 < 5 + tezina)
+		{
+			ucenici.get(0).polozioPraksu();
+			ucenici.remove(0);
+		}
+		else
+			ucenici.get(0).pao();
+	}
+	
+	@Override
+	public String toString()
+	{
+		String nesto = "";
+		nesto += "Naziv: " + naziv + "\n";
+		nesto += "Lokacija: " + lokacija + "\n";
+		nesto += "Cena: " + cena + "\n";
+		for(Ucenik ucenik:ucenici)
+		{
+			nesto += ucenik + "\n";
+		}
+		for(Instruktor instruktor:instruktori)
+		{
+			nesto += instruktor + "\n";
+		}
+		return nesto;
+	}
+}
---
+++ Auto-skola-master/com/autoskola/Main.java
@@ -0,0 +1,166 @@
+package com.autoskola;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import com.autoskola.FunkcijeZaFajl;
+import com.autoskola.Instruktor.TipInstruktora;
+import com.autoskola.Osoba.Pol;
+import com.autoskola.Osoba;
+import com.autoskola.Pneumatici.tip;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner unosScanner = new Scanner(System.in);
+        int izbor;
+        int izbor2;
+        do {
+            System.out.println("1 - Dodaj\n2 - Prikazi3 - Izmeni\n\n4 - Izadji");
+            izbor = unosScanner.nextInt();
+
+            switch (izbor) {
+                case 1:
+                    System.out.println(
+                            "Izabrali ste dodavanje. Sta zelite da dodate?\n\n1 - Novi Ucenik\n2 - Novi Instruktor\n3 - Novo Vozilo\n\n4 - Nazad");
+                    izbor2 = unosScanner.nextInt();
+                    switch (izbor2) {
+                        case 1:
+                            String ime;
+                            String prezime;
+                            LocalDate datumRodjenja;
+                            Pol pol;
+
+                            System.out.println("----------DODAVANJE NOVOG UCENIKA----------");
+                            System.out.println("Unesite Ime: ");
+                            ime = unosScanner.nextLine();
+                            System.out.println("Unesite Prezime: ");
+                            prezime = unosScanner.nextLine();
+                            System.out.println("Unesite Datum Rodjenja ()");
+                            // datumRodjenja = unosScanner. moram da nadjem koji scanner da koristim
+                            // pol = unosScanner. isto
+
+                            // dodajUcenika();
+                            break;
+                        case 2:
+                            String ime1;
+                            String prezime1;
+                            LocalDate datumRodjenja1;
+                            Pol pol1;
+                            int plata;
+                            TipInstruktora tipInstruktora;
+                            List<Ucenik> listaUcenika;
+
+                            System.out.println("----------DODAVANJE NOVOG INSTRUKTORA----------");
+                            System.out.println("Unesite Ime: ");
+                            ime = unosScanner.nextLine();
+                            System.out.println("Unesite Prezime: ");
+                            prezime = unosScanner.nextLine();
+                            System.out.println("Unesite Datum Rodjenja ()");
+                            // datumRodjenja = unosScanner. moram da nadjem koji scanner da koristim
+                            System.out.println("Unesite Pol (Muski, Zenski): ");
+                            // pol = unosScanner. isto
+                            System.out.println("Unesite Platu: ");
+                            plata = unosScanner.nextInt();
+                            System.out.println("Unesite Tip Istruktora (Teorija, Praksa): ");
+                            // tipInstruktora = unosScanner. enum glupi
+                            System.out.println("Unesite Ucenike ovog Instruktora: ");
+                            // list.add() ovo cu kasniije treba da napravim celu petlju
+
+                            // dodajInstruktora();
+                            break;
+                        case 3:
+                            int maxBrzina;
+                            String marka;
+                            String verzija;
+                            int godiste;
+                            String serijskiBroj;
+                            String boja;
+                            boolean automatik;
+                            int kilometraza;
+                            int rezervoar;
+                            int stanjeGoriva = 100;
+                            Motor motor;
+                            Pneumatici pneumatici;
+
+                            System.out.println("----------DODAVANJE NOVOG VOZILA----------");
+                            System.out.println("Unesite Maksimalnu Brzinu: ");
+                            maxBrzina = unosScanner.nextInt();
+                            System.out.println("Unesite Marku: ");
+                            marka = unosScanner.nextLine();
+                            System.out.println("Unesite Verziju: ");
+                            verzija = unosScanner.nextLine();
+                            System.out.println("Unesite Godiste: ");
+                            godiste = unosScanner.nextInt();
+                            System.out.println("Unesite Serijski broj: ");
+                            serijskiBroj = unosScanner.nextLine();
+                            System.out.println("Unesite Boju: ");
+                            boja = unosScanner.nextLine();
+                            System.out.println("Unseite Vrstu Menjaca (true - automatik, false - manualni): ");
+
+                            System.out.println("Unesite Kilometrazu: ");
+                            System.out.println("Unesite Velicinu Rezervoara: ");
+                            System.out.println("-----UNOSENJE MOTORA-----");
+                            System.out.println("-----UNOSENJE PNEUMATIKA-----");
+
+                            break;
+                        case 4:
+
+                            break;
+                    }
+                    break;
+                case 2:
+                    System.out.println(
+                            "Izabrali ste prikazivanje. Sta zelite da prikazete?\n\n1 - Prikazi Ucenike\n2 - Prikazi Instruktore\n3 - Prikazi Vozila\n\n4 - Nazad");
+                    izbor2 = unosScanner.nextInt();
+                    switch (izbor2) {
+                        case 1:
+
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;
+                        case 4:
+
+                            break;
+                    }
+                    break;
+                case 3:
+                    System.out.println(
+                            "Izabrali ste pizmenjivanje. Sta zelite da izmenite?\n\n1 - Izmeni Ucenika\n2 - Izmeni Instruktora\n3 - Izmeni Vozilo\n\n4 - Nazad");
+                    izbor2 = unosScanner.nextInt();
+                    switch (izbor2) {
+                        case 1:
+
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+
+                            break;
+                        case 4:
+
+                            break;
+                    }
+                    break;
+                case 4:
+                    System.out.println("Dovidjenja :)");
+                    return;
+            }
+        } while (izbor < 1 || izbor > 4);
+        FunkcijeZaFajl fajl = new FunkcijeZaFajl();
+        fajl.NapisiUFaljl("Provera");
+    }
+}
